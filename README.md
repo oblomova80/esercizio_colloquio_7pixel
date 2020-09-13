b45ac93f7c1699d301b837ac178ed5742bda6884
maria <maria@maria-zenbook>
2020-09-13 22:28:25 +0200

    Aggiunte funzioni per prendere input da esterno con validazione range

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Scrivere una funzione che dato un array V di N interi, restituisce il più piccolo degli interi tali che C > 0. Dove C e -C (che è l’opposto) esistano nell’array V.
 Se non ci sono interi, la funzione dovrebbe restituire 0.
+Scrivere un algoritmo efficiente preferibilmente con il requisito che C è un intero con un range [0, ...,
+100000] e che ciascuno elemento dell’array V possa avere un range [-100000,...,100000]
 
 Test
 ----
@@ -13,3 +15,5 @@ Per effettuare i test:
 
 Esecuzione:
 ------------
+
+> python3 src/main.py -l 11111 -11 -99999 -2  99999 11 2
--- src/main.py
+++ src/main.py
@@ -1,3 +1,24 @@
+import argparse
+
+LIMIT = 100000
+
+def main():
+	parser = argparse.ArgumentParser(add_help=True,
+                                	description='funzione per calcolare minimo intero in array dove è presente anche opposto')
+	parser.add_argument('-l', '--list', help='lista interi compresi tra -100000 e 100000',  nargs="+", type=int)
+	args = parser.parse_args()
+	input_array = [int(i) for i in args.list if in_range(int(i))]
+	result = esercizio(input_array)
+	print("Il più piccolo intero presente in array dato {} dove è presente opposto è {}".format(input_array, result))
+
+def in_range(i):
+	"""
+	controlla se l'intero è nel range stabilito
+    """
+	if int(i) not in range(-LIMIT,LIMIT):
+		raise Exception("Insertito intero non nel range corretto [-{limit}, {limit}]: {value}".format(limit = LIMIT, value = str(i)))
+	return True
+
 def esercizio(array):
 	"""
 	Ritorna il più piccolo intero
@@ -10,3 +31,7 @@ def esercizio(array):
 		if -x in array:
 			return x
 	return 0
+
+
+if __name__ == '__main__':
+    main()
